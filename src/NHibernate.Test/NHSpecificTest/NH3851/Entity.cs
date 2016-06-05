cf2775ab5387a4757e43cb7c0b4664d45736a641
victor.govorov <victor.govorov@engine6.com>
2016-06-06 01:03:36 +0500

    NH-3851 - added test

---
+++ src/NHibernate.Test/NHSpecificTest/NH3851/Entity.cs
@@ -0,0 +1,8 @@
+namespace NHibernate.Test.NHSpecificTest.NH3851
+{
+	public class Entity
+	{
+		public virtual string Foo { get; set; }
+		public virtual int Id { get; set; }
+	}
+}
---
+++ src/NHibernate.Test/NHSpecificTest/NH3851/Fixture.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using NHibernate.Linq;
+using NUnit.Framework;
+
+namespace NHibernate.Test.NHSpecificTest.NH3851
+{
+	[TestFixture]
+	public class Fixture : BugTestCase
+	{
+		public override string BugNumber
+		{
+			get { return "NH3851"; }
+		}
+
+		[Test]
+		public void LinqQuerySkipCount()
+		{
+			using (ISession session = OpenSession())
+			{
+				var count = session.Query<Entity>().Skip(100).Count();
+				Assert.That(count, Is.EqualTo(0));
+			}	
+		}
+	}
+}
